aa4f1ca131d682c72399f75346fa9aa085237c35
Jonas Schuepfer <jonasschuepfer@gmail.com>
2021-12-11 18:38:42 +0100

    Embracing templates

--- src/module.cpp
+++ src/module.cpp
@@ -1,7 +1,8 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
-#include <srrg_hbst/types/binary_tree.hpp>
+#include <srrg_hbst/types/binary_matchable.hpp>
 #include <srrg_hbst/types/binary_node.hpp>
+#include <srrg_hbst/types/binary_tree.hpp>
 
 namespace py = pybind11;
 
@@ -9,33 +10,58 @@ namespace py = pybind11;
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
 
-typedef srrg_hbst::BinaryTree128<uint64_t> _BinaryTree128;
-typedef srrg_hbst::BinaryTree256<uint64_t> _BinaryTree256;
-typedef srrg_hbst::BinaryTree512<uint64_t> _BinaryTree512;
-
-
-class BinaryTree256 : public _BinaryTree256 {
+template <typename BinaryNodeType_>
+class BinaryTreeAdapter : public srrg_hbst::BinaryTree<BinaryNodeType_> {
 public:
-    /* Inherit the constructors */
-    using _BinaryTree256::_BinaryTree256;
+    using BinaryTree = srrg_hbst::BinaryTree<BinaryNodeType_>;
+    using ObjectType = typename BinaryTree::ObjectType;
+    using Matchable = typename BinaryTree::Matchable;
+    using MatchableVector = typename BinaryTree::MatchableVector;
 
-    void add(uint64_t imageId, py::array_t<uint64_t> descriptorIds, py::array_t<uint64_t> descriptors, srrg_hbst::SplittingStrategy trainMode) {
-        BinaryTree256::MatchableVector matchables = std::move(buildMatchableVector(imageId, descriptorIds, descriptors));
-        _BinaryTree256::add(matchables, trainMode);
+    void add(ObjectType imageId, py::array_t<ObjectType> descriptorIds, py::array_t<uint8_t> descriptors, srrg_hbst::SplittingStrategy trainMode) {
+        MatchableVector matchables = std::move(buildMatchableVector(imageId, descriptorIds, descriptors));
+        BinaryTree::add(matchables, trainMode);
+    }
+
+    // std::vector<_BinaryMatchable256> match(py::array_t<uint8_t> queryDescriptors, uint32_t maximumDistance, bool lazy) {
+
+    //     if (lazy) {
+    //         matchLazy(, maximumDistance);
+    //     }
+    // }
+
+
+    static void bind(pybind11::module_& m, std::string name) {
+        using cls = BinaryTreeAdapter<BinaryNodeType_>;
+        py::class_<cls>(m, name.c_str())
+            .def(py::init<>())
+            .def("add", &cls::add)
+            .def("read", &cls::read)
+            .def("write", &cls::write)
+            .def("clear", &cls::clear)
+            .def("train", &cls::train)
+            .def("numberOfMatchablesUncompressed", &cls::numberOfMatchablesUncompressed)
+            .def("numberOfMatchablesCompressed", &cls::numberOfMatchablesCompressed)
+            .def("size", &cls::size);
     }
 
 private:
-    static BinaryTree256::MatchableVector buildMatchableVector(uint64_t imageId, py::array_t<uint64_t>& descriptorIds, py::array_t<uint64_t>& descriptors) {
+    static MatchableVector buildMatchableVector(ObjectType imageId, py::array_t<ObjectType>& descriptorIds, py::array_t<uint8_t>& descriptors) {
         if (descriptorIds.ndim() != 1) {
             throw std::runtime_error("Incompatible buffer shape for descriptor ids, expected 1d array");
         }
-        if (descriptors.ndim() != 1) {
-            throw std::runtime_error("Incompatible buffer shape for descriptors, expected 1d array");
+        if (descriptors.ndim() != 2) {
+            throw std::runtime_error("Incompatible buffer shape for descriptors, expected 2d array");
         }
+        // if (descriptors.ndim() != 2) {
+        //     throw std::runtime_error("Incompatible buffer shape for descriptors, expected 1d array");
+        // }
 
-        BinaryTree256::MatchableVector matchables(descriptorIds.shape(0));
+        // BinaryTree<BinaryNodeType_>::BinaryMatchable::descriptor_size_bits_in_bytes;
+
+        MatchableVector matchables(descriptorIds.shape(0));
         for (uint64_t i = 0; i < descriptorIds.shape(0); ++i) {
-            matchables[i] = new BinaryTree256::Matchable(descriptorIds.at(i), descriptors.at(i), imageId);
+            matchables[i] = new Matchable(descriptorIds.at(i), descriptors.at(i), imageId);
         }
 
         return matchables;
@@ -43,11 +69,10 @@ private:
 };
 
 
-int add(int i, int j) {
-    return i + j;
-}
+typedef BinaryTreeAdapter<srrg_hbst::BinaryNode128<uint64_t>> BinaryTreeAdapter128;
+typedef BinaryTreeAdapter<srrg_hbst::BinaryNode256<uint64_t>> BinaryTreeAdapter256;
+typedef BinaryTreeAdapter<srrg_hbst::BinaryNode512<uint64_t>> BinaryTreeAdapter512;
 
-namespace py = pybind11;
 
 PYBIND11_MODULE(hbst, m) {
     py::enum_<srrg_hbst::SplittingStrategy>(m, "SplittingStrategy")
@@ -57,21 +82,9 @@ PYBIND11_MODULE(hbst, m) {
         .value("SplitRandomUniform", srrg_hbst::SplittingStrategy::SplitRandomUniform)
         .export_values();
 
-    py::class_<BinaryTree256>(m, "BinaryTree256")
-        .def(py::init<>())
-        .def("add", &BinaryTree256::add)
-        .def("read", &BinaryTree256::read)
-        .def("write", &BinaryTree256::write)
-        .def("clear", &BinaryTree256::clear)
-        .def("train", &BinaryTree256::train)
-        .def("numberOfMatchablesUncompressed", &BinaryTree256::numberOfMatchablesUncompressed)
-        .def("numberOfMatchablesCompressed", &BinaryTree256::numberOfMatchablesCompressed)
-        .def("size", &BinaryTree256::size);
-
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-        Some other explanation about the add function.
-    )pbdoc");
+    BinaryTreeAdapter128::bind(m, "BinaryTree128");
+    BinaryTreeAdapter256::bind(m, "BinaryTree256");
+    BinaryTreeAdapter512::bind(m, "BinaryTree512");
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
