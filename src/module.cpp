cfc887623397fc7138206f28b01bbd3ef37daafc
Jonas Schuepfer <jonasschuepfer@gmail.com>
2021-12-11 19:44:42 +0100

    Preparing numpy uint8 vector to bitset conversion for descriptors

--- src/module.cpp
+++ src/module.cpp
@@ -3,6 +3,7 @@
 #include <srrg_hbst/types/binary_matchable.hpp>
 #include <srrg_hbst/types/binary_node.hpp>
 #include <srrg_hbst/types/binary_tree.hpp>
+#include <math.h>
 
 namespace py = pybind11;
 
@@ -17,9 +18,10 @@ public:
     using ObjectType = typename BinaryTree::ObjectType;
     using Matchable = typename BinaryTree::Matchable;
     using MatchableVector = typename BinaryTree::MatchableVector;
+    using Descriptor = typename BinaryTree::Descriptor;
 
-    void add(ObjectType imageId, py::array_t<ObjectType> descriptorIds, py::array_t<uint8_t> descriptors, srrg_hbst::SplittingStrategy trainMode) {
-        MatchableVector matchables = std::move(buildMatchableVector(imageId, descriptorIds, descriptors));
+    void add(uint16_t imageId, py::array_t<ObjectType> descriptorIds, py::array_t<uint8_t> descriptors, srrg_hbst::SplittingStrategy trainMode, bool padDescriptorsIfRequired) {
+        MatchableVector matchables = std::move(buildMatchableVector(imageId, descriptorIds, descriptors, padDescriptorsIfRequired));
         BinaryTree::add(matchables, trainMode);
     }
 
@@ -36,32 +38,57 @@ public:
         py::class_<cls>(m, name.c_str())
             .def(py::init<>())
             .def("add", &cls::add)
+            .def("train", &cls::train)
+            .def("clear", &cls::clear)
             .def("read", &cls::read)
             .def("write", &cls::write)
-            .def("clear", &cls::clear)
-            .def("train", &cls::train)
             .def("numberOfMatchablesUncompressed", &cls::numberOfMatchablesUncompressed)
             .def("numberOfMatchablesCompressed", &cls::numberOfMatchablesCompressed)
             .def("size", &cls::size);
     }
 
 private:
-    static MatchableVector buildMatchableVector(ObjectType imageId, py::array_t<ObjectType>& descriptorIds, py::array_t<uint8_t>& descriptors) {
+    static Descriptor buildDescriptor(const u_char* descriptor) {
+        // see getDescriptor for SRRG_HBST_HAS_OPENCV in binary_matchable.hpp
+        Descriptor binaryDescriptor; // padding is done implicitely, instantiation zeros all bits
+
+        for (uint64_t byteIndex = 0; byteIndex < Matchable::raw_descriptor_size_bytes; ++byteIndex) {
+            const uint32_t bitIndexStart = byteIndex * 8;
+            const std::bitset<8> descriptorByte(descriptor[byteIndex]);
+            for (uint8_t v = 0; v < 8; ++v) {
+                binaryDescriptor[bitIndexStart + v] = descriptorByte[v];
+            }
+        }
+
+        // in case the last byte is not fully used
+        if (Matchable::descriptor_size_bits_overflow > 0) {
+            const std::bitset<8> descriptorByte(descriptor[Matchable::raw_descriptor_size_bytes]);
+            for (uint32_t v = 0; v < Matchable::descriptor_size_bits_overflow; ++v) {
+                binaryDescriptor[Matchable::descriptor_size_bits_in_bytes + v] = descriptorByte[8 - Matchable::descriptor_size_bits_overflow + v];
+            }
+        }
+        return binaryDescriptor;
+    }
+
+    static MatchableVector buildMatchableVector(uint64_t imageId, py::array_t<ObjectType>& descriptorIds, py::array_t<uint8_t>& descriptors, bool pad) {
         if (descriptorIds.ndim() != 1) {
             throw std::runtime_error("Incompatible buffer shape for descriptor ids, expected 1d array");
         }
         if (descriptors.ndim() != 2) {
             throw std::runtime_error("Incompatible buffer shape for descriptors, expected 2d array");
         }
-        // if (descriptors.ndim() != 2) {
-        //     throw std::runtime_error("Incompatible buffer shape for descriptors, expected 1d array");
-        // }
-
-        // BinaryTree<BinaryNodeType_>::BinaryMatchable::descriptor_size_bits_in_bytes;
+        uint32_t expectedByteCount = ceil(Matchable::descriptor_size_bits / 8.);
+        if (!pad && descriptors.shape(1) != expectedByteCount) {
+            throw std::runtime_error("Incompatible buffer shape for descriptors, dimension 0: number of descriptors, dimension 1: " + std::to_string(expectedByteCount));
+        }
+        if (pad && descriptors.shape(1) > expectedByteCount) {
+            throw std::runtime_error("Incompatible buffer shape for descriptors, dimension 0: number of descriptors, dimension 1: <=" + std::to_string(expectedByteCount));
+        }
 
         MatchableVector matchables(descriptorIds.shape(0));
         for (uint64_t i = 0; i < descriptorIds.shape(0); ++i) {
-            matchables[i] = new Matchable(descriptorIds.at(i), descriptors.at(i), imageId);
+            Descriptor d = buildDescriptor(descriptors.data(i));
+            matchables[i] = new Matchable(descriptorIds.at(i), d, imageId); // TODO: turn uint8 array row into a bitset
         }
 
         return matchables;
@@ -83,8 +110,8 @@ PYBIND11_MODULE(hbst, m) {
         .export_values();
 
     BinaryTreeAdapter128::bind(m, "BinaryTree128");
-    BinaryTreeAdapter256::bind(m, "BinaryTree256");
-    BinaryTreeAdapter512::bind(m, "BinaryTree512");
+    // BinaryTreeAdapter256::bind(m, "BinaryTree256");
+    // BinaryTreeAdapter512::bind(m, "BinaryTree512");
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
